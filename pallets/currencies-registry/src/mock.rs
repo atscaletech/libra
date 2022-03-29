4746ea60c790ad1e83d98ddbd25c8feeedc034d1
Andrew Le <andrew@atscale.xyz>
2022-03-29 17:08:41 +0700

    test: currencies registry

--- pallets/currencies-registry/src/mock.rs
+++ pallets/currencies-registry/src/mock.rs
@@ -1,26 +1,22 @@
-use crate as currency_registry;
-use frame_support::parameter_types;
+use crate as currencies_registry;
+use frame_support::{construct_runtime, parameter_types};
 use frame_system as system;
+pub use pallet_balances::Call as BalancesCall;
 use sp_core::H256;
 use sp_runtime::{
-	testing::Header,
+	generic,
 	traits::{BlakeTwo256, IdentityLookup},
 };
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
-type Block = frame_system::mocking::MockBlock<Runtime>;
+pub type BlockNumber = u64;
+pub type AccountId = u128;
+pub type Balance = u128;
+pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+pub type Hash = H256;
 
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Runtime where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		CurrencyRegistry: currency_registry::{Pallet, Call, Storage, Event<T>},
-	}
-);
+pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+pub const BONDING_AMOUNT: Balance = 100;
 
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
@@ -35,17 +31,17 @@ impl system::Config for Runtime {
 	type Origin = Origin;
 	type Call = Call;
 	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
+	type BlockNumber = BlockNumber;
+	type Hash = Hash;
 	type Hashing = BlakeTwo256;
-	type AccountId = u64;
+	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type Event = Event;
 	type BlockHashCount = BlockHashCount;
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -54,14 +50,64 @@ impl system::Config for Runtime {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
-impl currency_registry::Config for Runtime {
-	type Event = Event;
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 500;
+	pub const MaxLocks: u32 = 50;
 }
+
+impl pallet_balances::Config for Runtime {
+	type MaxLocks = MaxLocks;
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = frame_system::Pallet<Runtime>;
+	type WeightInfo = pallet_balances::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
+	pub const BondingAmount: Balance = BONDING_AMOUNT;
+}
+
+impl currencies_registry::Config for Runtime {
+	type Event = Event;
+	type Currency = Balances;
+	type BondingAmount = BondingAmount;
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		CurrenciesRegistry: currencies_registry::{Pallet, Call, Storage, Event<T>},
+	}
+);
 
 pub struct ExtBuilder;
 
 impl ExtBuilder {
 	pub fn build(self) -> sp_io::TestExternalities {
-		system::GenesisConfig::default().build_storage::<Runtime>().unwrap().into()
+		let mut t = system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+
+		pallet_balances::GenesisConfig::<Runtime> {
+			balances: vec![(ALICE, 1_000), (BOB, 1_000)],
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		t.into()
 	}
+}
+
+pub fn last_event() -> Event {
+	system::Pallet::<Runtime>::events().pop().expect("Event expected").event
 }
--- pallets/currencies-registry/src/tests.rs
+++ pallets/currencies-registry/src/tests.rs
@@ -1,15 +1,127 @@
-use crate::{mock::*};
+use super::*;
+
 use frame_support::{assert_noop, assert_ok};
+use frame_system as system;
+use mock::{
+	last_event, CurrenciesRegistry, Event, ExtBuilder, Origin, Runtime, System, ALICE, BOB,
+};
+use sp_core::H256;
+use sp_runtime::traits::Hash;
+
+fn expected() -> (<Runtime as system::Config>::Hash, CurrencyMetadata<Runtime>) {
+	let metadata = CurrencyMetadata::<Runtime> {
+		name: "Polkadot".into(),
+		symbol: "dot".into(),
+		decimals: 12_u8,
+		issuer: ALICE,
+	};
+
+	let currency_id = <Runtime as system::Config>::Hashing::hash_of(&metadata);
+
+	(currency_id, metadata)
+}
 
 #[test]
-fn it_works_for_default_value() {
+fn create_currency_works() {
 	ExtBuilder.build().execute_with(|| {
+		System::set_block_number(1);
+
+		let (currency_id, metadata) = expected();
+
+		assert_ok!(CurrenciesRegistry::create_currency(
+			Origin::signed(ALICE),
+			"Polkadot".into(),
+			"dot".into(),
+			12,
+		));
+		assert_eq!(
+			last_event(),
+			Event::CurrenciesRegistry(crate::Event::CurrencyCreated {
+				currency_id,
+				created_by: ALICE,
+			}),
+		);
+		assert_eq!(CurrenciesRegistry::currencies(currency_id).unwrap(), metadata);
+
+		assert_noop!(
+			CurrenciesRegistry::create_currency(
+				Origin::signed(ALICE),
+				"Polkadot".into(),
+				"dot".into(),
+				12,
+			),
+			Error::<Runtime>::CurrencyExisted
+		);
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn remove_currency_works() {
 	ExtBuilder.build().execute_with(|| {
+		System::set_block_number(1);
 
+		let (currency_id, metadata) = expected();
+
+		assert_ok!(CurrenciesRegistry::create_currency(
+			Origin::signed(ALICE),
+			"Polkadot".into(),
+			"dot".into(),
+			12,
+		));
+
+		assert_noop!(
+			CurrenciesRegistry::remove_currency(Origin::signed(ALICE), H256::zero()),
+			Error::<Runtime>::CurrencyNotFound
+		);
+		assert_noop!(
+			CurrenciesRegistry::remove_currency(Origin::signed(BOB), currency_id,),
+			Error::<Runtime>::NotCurrencyIssuer
+		);
+
+		assert_ok!(CurrenciesRegistry::remove_currency(Origin::signed(ALICE), currency_id));
+		assert_eq!(
+			last_event(),
+			Event::CurrenciesRegistry(crate::Event::CurrencyRemoved {
+				currency_id,
+				name: metadata.name,
+				symbol: metadata.symbol,
+				decimals: 12,
+				removed_by: ALICE,
+			}),
+		);
+
+		assert_noop!(
+			CurrenciesRegistry::remove_currency(Origin::signed(ALICE), currency_id),
+			Error::<Runtime>::CurrencyNotFound
+		);
+	});
+}
+
+#[test]
+fn accept_currency_works() {
+	ExtBuilder.build().execute_with(|| {
+		System::set_block_number(1);
+		let (currency_id) = expected();
+
+		assert_ok!(CurrenciesRegistry::create_currency(
+			Origin::signed(ALICE),
+			"Polkadot".into(),
+			"dot".into(),
+			12,
+		));
+		assert_noop!(
+			CurrenciesRegistry::accept_currency(Origin::signed(ALICE), H256::zero()),
+			Error::<Runtime>::CurrencyNotFound
+		);
+		assert_ok!(CurrenciesRegistry::accept_currency(Origin::signed(BOB), currency_id,));
+		assert_eq!(
+			last_event(),
+			Event::CurrenciesRegistry(crate::Event::CurrencyAccepted {
+				currency_id,
+				accepted_by: BOB,
+			}),
+		);
+		assert_eq!(CurrenciesRegistry::accepted_currencies(BOB).len(), 1);
+		assert_eq!(CurrenciesRegistry::accepted_currencies(BOB)[0], currency_id);
 	});
 }
