0a95f7aae48c6ef15f1fca214da9ddf303141b97
Andrew Le <andrew@atscale.xyz>
2022-07-12 17:09:58 +0700

    feat: update runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -84,17 +84,17 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("libra-runtime"),
 	impl_name: create_runtime_str!("libra-runtime"),
-	authoring_version: 3,
+	authoring_version: 2,
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
 	spec_version: 102,
-	impl_version: 3,
+	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
-	state_version: 2,
+	transaction_version: 2,
+	state_version: 1,
 };
 
 /// This determines the average expected block time that we are targeting.
